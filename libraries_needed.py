f52106310d03d97ca6d74cdec8a499ae13d54c58
ErickCuellar1 <erick.cuellar.2101@gmail.com>
2025-09-23 20:01:26 -0600

    Updated week 3 info

---
+++ libraries_needed.py
@@ -0,0 +1,9 @@
+import scipy
+import sklearn # pip install scikit-learn
+import pymysql
+import pandas as pd
+import seaborn as sns
+import firebase_admin
+import sqlalchemy as sqla
+import matplotlib.pyplot as plt
+from category_encoders.binary import BinaryEncoder
